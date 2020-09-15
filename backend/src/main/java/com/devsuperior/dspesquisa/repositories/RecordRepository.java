22fa3c24e541edf8591d03a60329043be86fc91f
Gustavo Xavier <guh.coostta@gmail.com>
2020-09-15 09:32:27 -0300

    DTO, Service

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -1,9 +1,11 @@
 package com.devsuperior.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dspesquisa.entities.Game;
 
+@Repository
 public interface RecordRepository extends JpaRepository<Game, Long> {
 	
 }
