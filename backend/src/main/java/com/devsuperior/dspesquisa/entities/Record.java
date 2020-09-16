dff41040512c8c2275391f2eb7863c69b97d7ba7
Gustavo Xavier <guh.coostta@gmail.com>
2020-09-16 17:31:46 -0300

    Ready for production

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Record.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Record.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_Record")
+@Table(name = "tb_record")
 public class Record implements Serializable {
 	private static final long serialVersionUID = 1L;
 
