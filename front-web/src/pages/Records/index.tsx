f4e7a707138d3c996f31f0591aaf1898f52fbbbb
Gustavo Xavier <guh.coostta@gmail.com>
2020-09-18 22:02:37 -0300

    Adicionando a página de gráficos

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -4,7 +4,9 @@ import './styles.css'
 import { RecordsResponse } from './types'
 import { formatDate } from './helpers'
 import Pagination from './Pagination'
-import { Link } from 'react-router-dom'
+import Filters from '../../components/Filters'
+
+
 
 const BASE_URL = 'http://localhost:8080'
 
@@ -23,45 +25,41 @@ const Records = () => {
 
     return (
         <div className="page-container">
-            <div className="filters-container records-actions">
-                <Link to="/charts">
-                    <button className="action-filters">
-                        Ver Grafico
-                    </button>
-                </Link>
-            </div>
-        <table className="records-table" cellPadding="0" cellSpacing="0">
-            <thead> 
-                <tr>
-                    <th>INSTANTE</th>
-                    <th>NOME</th>
-                    <th>IDADE</th>
-                    <th>PLATAFORMA</th>
-                    <th>GENERO</th>
-                    <th>TITULO DO GAME</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                {recordsResponse?.content.map(record => (
-                    <tr key={record.id}>
-                        <td>{formatDate(record.moment)}</td>
-                        <td>{record.name}</td>
-                        <td>{record.age}</td>
-                        <td className="text-secondary">{record.gamePlatform}</td>
-                        <td>{record.genreName}</td>
-                        <td className="text-primary">{record.gameTitle}</td>
+            
+            <Filters link="/charts" linkText="VER GRÁFICO"/>
+            
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead> 
+                    <tr>
+                        <th>INSTANTE</th>
+                        <th>NOME</th>
+                        <th>IDADE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GENERO</th>
+                        <th>TITULO DO GAME</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
-        <Pagination 
-            activePage={activePage}
-            goToPage={handlePageChange}
-            totalPages={recordsResponse?.totalPages}
+                </thead>
 
-        />
-    </div>
+                <tbody>
+                    {recordsResponse?.content.map(record => (
+                        <tr key={record.id}>
+                            <td>{formatDate(record.moment)}</td>
+                            <td>{record.name}</td>
+                            <td>{record.age}</td>
+                            <td className="text-secondary">{record.gamePlatform}</td>
+                            <td>{record.genreName}</td>
+                            <td className="text-primary">{record.gameTitle}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <Pagination 
+                activePage={activePage}
+                goToPage={handlePageChange}
+                totalPages={recordsResponse?.totalPages}
+
+            />
+        </div>
     )
 }
 
